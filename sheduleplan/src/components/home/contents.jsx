0d79dc85ba78c111251fa71b0338db3dcae11b0d
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-13 21:36:29 +0530

    Create contents.jsx

---
+++ sheduleplan/src/components/home/contents.jsx
@@ -0,0 +1,13 @@
+export default function Contents(){
+    return(
+        <div className="bg-[#181818]">
+            <div className="container py-5">
+                <h2 className="text-white text-center font-normal text-[52px] leading-[62px] tracking-[0.2px]">Contents</h2>
+            </div>
+            <div className="w-[600px] pt-5 h-[80px] mx-auto px-5 text-center text-white font-normal text-xl tracking-[0.2px]">
+                <h3>We focus on ergonomics and meeting you where you work. It's only a keystroke away.</h3>
+            </div>
+        </div>
+        
+    );
+}
