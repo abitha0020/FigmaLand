c35d86262127e5e2267c94e3cba5db048353e377
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-13 20:31:18 +0530

    Update home.jsx

--- sheduleplan/src/pages/home.jsx
+++ sheduleplan/src/pages/home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import Homeintro from "../components/home/home-intro";
+import Features from "../components/home/features";
+import Contents from "../components/home/contents";
 export default function Home(){
     return(
-        <div class="bg-black">
+        <div className="bg-black">
             <Navbar />
+            <Homeintro/>   
+            <Features />
+            <Contents />
         </div>
     );
     
