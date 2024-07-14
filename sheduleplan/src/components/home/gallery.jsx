9403533d579f0f2d2de6526ba04053b9ca4fdf36
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-14 21:58:16 +0530

    Update gallery.jsx

--- sheduleplan/src/components/home/gallery.jsx
+++ sheduleplan/src/components/home/gallery.jsx
@@ -1,12 +1,10 @@
+import Title from "./title";
 export default function Gallery() {
     return(
         <div className="p-10">
-            <div className="container py-5 mt-20">
-                <h2 className="text-white text-center font-normal text-[52px] leading-[62px] tracking-[0.2px]">Gallery</h2>
-            </div>
-            <div className="w-[600px] pt-5 h-[80px] mx-auto px-5 text-center text-white font-normal text-xl tracking-[0.2px]">
-                <h3>We focus on ergonomics and meeting you where you work. It's only a keystroke away.</h3>
-            </div>
+            <Title 
+              title="Gallery"
+              subtitle="We focus on ergonomics and meeting you where you work. It's only a keystroke away."/>
             <div className="mx-auto  mt-14 w-[1000px] h-[285px] flex place-content-between">
                 <img src="src/assets/galleryimage.png" alt="" className="w-[225px] h-[285px] rounded-[12px]"/>
                 <img src="src/assets/tab.png" alt="" className="w-[225px] h-[285px] rounded-[12px]"/>
