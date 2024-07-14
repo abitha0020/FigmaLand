05f8694c785b59706c282e7f64d3b7f2fe09651d
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-14 21:54:23 +0530

    Update contents.jsx

--- sheduleplan/src/components/home/contents.jsx
+++ sheduleplan/src/components/home/contents.jsx
@@ -1,12 +1,10 @@
+import Title from "./title.jsx";
 export default function Contents(){
     return(
         <div >
-            <div className="container py-5 mt-20">
-                <h2 className="text-white text-center font-normal text-[52px] leading-[62px] tracking-[0.2px]">Contents</h2>
-            </div>
-            <div className="w-[600px] pt-5 h-[80px] mx-auto px-5 text-center text-white font-normal text-xl tracking-[0.2px]">
-                <h3>We focus on ergonomics and meeting you where you work. It's only a keystroke away.</h3>
-            </div>
+            <Title 
+              title="Contents"
+              subtitle="We focus on ergonomics and meeting you where you work. It's only a keystroke away."/>
             <div className="pt-14 w-[1000px] mx-auto flex place-content-between">
                 <div className="p-10 w-[475px] h-[625px] rounded-[10px] flex flex-col justify-between bg-white ">
                     <div>
