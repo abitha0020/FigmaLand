5e9349c9292266ef7626747b9489030dd85e044a
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-14 21:33:07 +0530

    gallery section images fixed

--- sheduleplan/src/components/home/gallery.jsx
+++ sheduleplan/src/components/home/gallery.jsx
@@ -1,10 +1,3 @@
-import Galleryimage from "./images/galleryimage.png";
-import Tab from "./images/tab.png";
-import People from "./images/people.png";
-import Tv from "./images/tv.png";
-import Codingimage from "./images/coding.png";
-import Laptop from "./images/laptop.png";
-import camera from "./images/camera.png";
 export default function Gallery() {
     return(
         <div className="p-10">
@@ -15,15 +8,15 @@ export default function Gallery() {
                 <h3>We focus on ergonomics and meeting you where you work. It's only a keystroke away.</h3>
             </div>
             <div className="mx-auto  mt-14 w-[1000px] h-[285px] flex place-content-between">
-                <img src={Galleryimage} alt="" className="w-[225px] h-[285px] rounded-[12px]"/>
-                <img src={Tab} alt="" className="w-[225px] h-[285px] rounded-[12px]"/>
-                <img src={People} alt="" className="w-[225px] h-[285px] rounded-[12px]"/>
-                <img src={Tv} alt="" className="w-[225px] h-[285px] rounded-[12px]"/>
+                <img src="src/assets/galleryimage.png" alt="" className="w-[225px] h-[285px] rounded-[12px]"/>
+                <img src="src/assets/tab.png" alt="" className="w-[225px] h-[285px] rounded-[12px]"/>
+                <img src="src/assets/people.png" alt="" className="w-[225px] h-[285px] rounded-[12px]"/>
+                <img src="src/assets/tv.png" alt="" className="w-[225px] h-[285px] rounded-[12px]"/>
             </div>
             <div className="mx-auto mt-8 w-[1090px] h-[285px] flex place-content-between">
-                <img src={Codingimage} alt="" className="w-[395px] h-[285px] rounded-[12px]" />
-                <img src={Laptop} alt="" className="w-[225px] h-[285px] rounded-[12px]"/>
-                <img src={camera} alt="" className="w-[401px] h-[285px] rounded-[12px]"/>
+                <img src="src/assets/coding.png" alt="" className="w-[395px] h-[285px] rounded-[12px]" />
+                <img src="src/assets/laptop.png" alt="" className="w-[225px] h-[285px] rounded-[12px]"/>
+                <img src="src/assets/camera.png" alt="" className="w-[401px] h-[285px] rounded-[12px]"/>
             </div>
             <div className="mt-10 mx-auto w-[160px] h-[52px]">
                 <button className="w-[160px] h-[52px] border border-white text-white font-medium text-base tracking-[0.2px] ">See More</button>
