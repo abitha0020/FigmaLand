f879e874cb87f9310b8aa942d51c367f00d7967d
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-14 21:56:34 +0530

    Update features.jsx

--- sheduleplan/src/components/home/features.jsx
+++ sheduleplan/src/components/home/features.jsx
@@ -1,12 +1,10 @@
+import Title from "./title.jsx";
 function Features(){
     return(
         <div className="bg-[#181818] py-10">
-            <div className="container py-5">
-                <h2 className="text-white text-center font-normal text-[52px] leading-[62px] tracking-[0.2px]">FEATURES</h2>
-            </div>
-            <div className="w-[600px] pt-5 h-[60px] mx-auto px-5 text-center text-white font-normal text-xl tracking-[0.2px]">
-                <h3>Most calendars are designed for teams. Slate is designed for freelancers who want a simple way to plan their schedule.</h3>
-            </div>
+            <Title 
+              title="FEATURES"
+              subtitle="Most calendars are designed for teams. Slate is designed for freelancers who want a simple way to plan their schedule."/>
             <div className="py-20 w-[80%] h-[800px] mx-auto flex">
                 <div className="column-8">
                     <img src="src/assets/Features.png" alt="Features Image"className="" />
